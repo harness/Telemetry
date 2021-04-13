377a22a2991d77c4767954cc174c983a613ae181
Nathan Nguyen <lenathan.nguyen@harness.io>
2021-04-13 10:43:34 -0600

    fix: used the script method

--- src/SegmentAnalytics.js
+++
@@ -1,8 +0,0 @@
-import Analytics from '@segment/analytics.js-core/build/analytics'
-
-// Hack to get around typescript errors from @segment/analytics.js-core/build/analytics misconfiguration
-function getNewAnalytics() {
-  return new Analytics()
-}
-
-export default getNewAnalytics
