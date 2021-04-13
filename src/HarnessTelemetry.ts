45d18a25fcc4bec27e6626045a823612175c43a7
Nathan Nguyen <lenathan.nguyen@harness.io>
2021-04-13 11:22:07 -0600

    AWEF

---
+++ src/HarnessTelemetry.ts
@@ -0,0 +1,73 @@
+import Track from './types/Track'
+import Page from './types/Page'
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    analytics: any
+  }
+}
+
+function isInitialized(): boolean {
+  return typeof window.analytics !== 'undefined' && typeof window.analytics?.initialize !== 'undefined'
+}
+
+class HarnessTelemetry {
+  analytics: Window['analytics']
+  initialized: boolean
+  statusInterval?: number
+
+  constructor() {
+    this.analytics = window.analytics
+    this.initialized = isInitialized()
+
+    if (!this.initialized && typeof window !== 'undefined') {
+      this.statusInterval = window.setInterval(this.updateInitializedStatus, 1000)
+    }
+  }
+
+  updateInitializedStatus(): void {
+    if (isInitialized()) {
+      this.initialized = true
+      this.analytics = window.analytics
+    }
+
+    clearInterval(this.statusInterval)
+  }
+
+  checkInitialized(): boolean {
+    if (this.initialized) {
+      return true
+    }
+
+    // eslint-disable-next-line no-console
+    console.error('The telemetry package needs to be initialized before calling analytics methods')
+
+    return false
+  }
+
+  identify(userId: string): void {
+    if (this.checkInitialized()) {
+      this.analytics.identify(userId, null, {}, {})
+    }
+  }
+
+  track(track: Track): void {
+    if (this.checkInitialized()) {
+      const { event, properties } = track
+
+      this.analytics.track(event, properties, {}, {})
+    }
+  }
+
+  page(page: Page): void {
+    if (this.checkInitialized()) {
+      const { name, category, properties } = page
+      this.analytics.page(category, name, properties, {}, {})
+    }
+  }
+}
+
+const Telemetry = new HarnessTelemetry()
+
+export default Telemetry
