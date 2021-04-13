41aecd6e0c2b9b99af8d3d77d54c167ca3280920
Nathan Nguyen <lenathan.nguyen@harness.io>
2021-04-13 10:43:34 -0600

    fix: used the script method

--- src/SegmentIntegration.js
+++
@@ -1,6 +0,0 @@
-import SegmentIntegration from '@segment/analytics.js-integration-segmentio'
-
-// Hack to get around typescript errors from @segment/analytics.js-integration-segmentio misconfiguration
-const integration = SegmentIntegration
-
-export default integration
