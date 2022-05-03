b9aa87b1553a06e6464998e6a15b54ec35972d1e
Sagnik De <sagnik.de@harness.io>
2022-05-03 15:19:27 -0400

    fix: [PLG-921]: Jenkins refernces removed from scripts (#6)

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,8 +13,6 @@ export VERSION=$(awk '/version/{gsub(/("|",)/,"",$2);print $2};' package.json)
 set -e
 
 echo @harness:registry=https://npm.pkg.github.com > ~/.npmrc
-# GITHUB_HARNESS_BOT_TOKEN is a secret line from Jenkins (npm.pkg.github.com/:_authToken=XXX)
-echo $GITHUB_HARNESS_BOT_TOKEN >> ~/.npmrc
 echo always-auth=true >> ~/.npmrc
 
 # Create a new branch like v1.0.1
@@ -27,11 +25,7 @@ yarn build
 git add -f ./dist/*
 git commit . -m"Release version: v$VERSION"
 
-# mkdir -p /jenkins-storage/ui/uikit/$NEW_VERSION
-# cp -R ./* /jenkins-storage/ui/uikit/$NEW_VERSION
 
-# This is for Jenkins to set environmen variables
-echo "VERSION=$VERSION" > build.properties
 
 echo "Publishing... v$VERSION"
 npm publish
